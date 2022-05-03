407ef272525b669ba698e84506e7e02f627c9a85
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2022-05-03 10:42:27 +0200

    added basic files

---
+++ train.py
@@ -0,0 +1,177 @@
+"""
+Main file for training Yolo model on Pascal VOC dataset
+
+"""
+import os
+import argparse
+import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torchvision.transforms.functional as FT
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from model import Yolov1
+from dataset import VOCDataset
+from utils import (
+    non_max_suppression,
+    mean_average_precision,
+    intersection_over_union,
+    cellboxes_to_boxes,
+    get_bboxes,
+    plot_image,
+    save_checkpoint,
+    load_checkpoint,
+    parse_cfg
+)
+from loss import YoloLoss
+
+seed = 123
+torch.manual_seed(seed)
+
+parser = argparse.ArgumentParser(description='YOLOv1-pytorch')
+parser.add_argument("--cfg", "-c", default="cfg/yolov1.yaml", help="Yolov1 config file path", type=str)
+parser.add_argument("--dataset_cfg", "-d", default="cfg/dataset.yaml", help="Dataset config file path", type=str)
+parser.add_argument("--epochs", "-e", default=1000, help="Training epochs", type=int)
+parser.add_argument("--batch_size", "-bs", default=16, help="Training batch size", type=int)
+parser.add_argument("--lr", "-lr", default=2e-5, help="Training learning rate", type=float)
+parser.add_argument("--load_model", "-lm", default='True', help="Load Model or train one [ 'True' | 'False' ]", type=str)  
+parser.add_argument("--model_path", "-mp", default="overfit.pth.tar", help="Model path", type=str)
+
+args = parser.parse_args()
+
+# Hyperparameters etc. 
+LEARNING_RATE = args.lr
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+BATCH_SIZE = args.batch_size # 64 in original paper but I don't have that much vram, grad accum?
+WEIGHT_DECAY = 0
+EPOCHS = args.epochs
+NUM_WORKERS = 2
+PIN_MEMORY = True
+if args.load_model == 'True':
+    LOAD_MODEL = True # DEFAULT MODUS: training
+elif args.load_model == 'False':
+    LOAD_MODEL = False
+LOAD_MODEL_FILE = args.model_path
+# IMG_DIR = "data/VOC2007_2012/images"
+# LABEL_DIR = "data/VOC2007_2012/labels"
+
+OUTPUT = open('output', 'w')
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, bboxes):
+        for t in self.transforms:
+            img, bboxes = t(img), bboxes
+
+        return img, bboxes
+
+
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+
+
+def train_fn(train_loader, model, optimizer, loss_fn):
+    loop = tqdm(train_loader, leave=True)
+    mean_loss = []
+
+    for batch_idx, (x, y) in enumerate(loop):
+        x, y = x.to(DEVICE), y.to(DEVICE)
+        out = model(x)
+        loss = loss_fn(out, y)
+        mean_loss.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # update progress bar
+        loop.set_postfix(loss=loss.item())
+
+    OUTPUT.write(f"Mean loss was {sum(mean_loss)/len(mean_loss)}\n")
+    print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
+
+
+def main():
+
+    print('Working Device: ', DEVICE)    cfg = parse_cfg(args.cfg)
+    S, B, num_classes, input_size = cfg['S'], cfg['B'], cfg['num_classes'], cfg['input_size']
+    dataset_cfg = parse_cfg(args.dataset_cfg)
+    IMG_DIR, LABEL_DIR = dataset_cfg['images'], dataset_cfg['labels']
+
+
+    model = Yolov1(split_size=S, num_boxes=B, num_classes=num_classes).to(DEVICE)
+    optimizer = optim.Adam(
+        model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
+    )
+    loss_fn = YoloLoss()
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
+
+    train_dataset = VOCDataset(
+        "data/VOC2007_2012/100examples.csv",
+        transform=transform,
+        img_dir=IMG_DIR,
+        label_dir=LABEL_DIR,
+    )
+
+    test_dataset = VOCDataset(
+        "data/VOC2007_2012/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
+    )
+
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+        pin_memory=PIN_MEMORY,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+        pin_memory=PIN_MEMORY,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    for epoch in range(EPOCHS):
+        if LOAD_MODEL == False:
+            for x, y in train_loader:
+                x = x.to(DEVICE)
+                for idx in range(8):
+                    bboxes = cellboxes_to_boxes(model(x))
+                    bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
+                    plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
+
+                import sys
+                sys.exit()
+
+        pred_boxes, target_boxes = get_bboxes(
+            train_loader, model, iou_threshold=0.5, threshold=0.4
+        )
+
+        mean_avg_prec = mean_average_precision(
+            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
+        )
+        OUTPUT.write(f"Train mAP: {mean_avg_prec}\n")
+        print(f"Train mAP: {mean_avg_prec}")
+
+        if mean_avg_prec > 0.9:
+            checkpoint = {
+                "state_dict": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+            }
+            save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
+            import time
+            sys.exit()
+            time.sleep(10)
+
+        train_fn(train_loader, model, optimizer, loss_fn)
+
+
+if __name__ == "__main__":
+    main()
