d0fdcaba524a78539fd4b7c9d78ef3d7e1e6d480
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2022-06-03 12:19:03 +0200

    comment added

--- model.py
+++ model.py
@@ -117,5 +117,5 @@ class Yolov1(nn.Module):
             nn.Linear(1024 * S * S, 496),
             nn.Dropout(0.0),
             nn.LeakyReLU(0.1),
-            nn.Linear(496, S * S * (C + B * 5)),
+            nn.Linear(496, S * S * (C + B * 5)), # classification layer?
         )
